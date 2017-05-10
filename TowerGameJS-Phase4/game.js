8072d236d73924147fd181de6919ad6f89c46f59
Charles Taussig <785022@seq.org>
2017-05-10 12:59:15 -0700

    things
    things

--- TowerGameJS-Phase4/game.js
+++ TowerGameJS-Phase4/game.js
@@ -383,8 +383,8 @@ class Game {
     for(var i = 0; i < 5; i++){
       var mtd = document.createElement("div"); // createDiv("");
       var h5 = document.createTextNode("Cost");
-      var cnvTurImgPath = "tow" + (i+1) + "s.png";  // small tower image for canvas
-      var cnvBulImgPath = "b" + (i+1) + ".png";     // bullet image for canvas
+      var cnvTurImgPath = "images/tow" + (i+1) + "s.png";  // small tower image for canvas
+      var cnvBulImgPath = "images/b" + (i+1) + ".png";     // bullet image for canvas
       mtd.cnvTurImg = new Image();
       mtd.cnvTurImg.addEventListener('load',this.hideImgElement,false);
       mtd.cnvTurImg.addEventListener('error', function() { console.log(cnvTurImgPath + " failed to load"); }, false);
@@ -401,7 +401,7 @@ class Game {
       mtd.cost = 100*i +50;
       mtd.id = 'towImgDiv' + i;
       tiles.push(mtd);
-      var imgName = 'tow' + i + '.png'; // large image for menu tile
+      var imgName = 'images/tow' + i + '.png'; // large image for menu tile
       var tImg = new Image();
       tImg.addEventListener('error', function() { console.log(imgName + " failed to load"); }, false);
       tImg.src = imgName;
